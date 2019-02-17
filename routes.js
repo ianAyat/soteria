8863cee36de7df85134cc6137e2097457569be26
ryan <phantomstation2012@gmail.com>
2019-02-17 13:58:17 +0800

    updated the routes.js file

--- routes.js
+++ routes.js
@@ -1,10 +1,17 @@
 var express = require('express');
 
+var verification = require('./messenger/verification')
+var message = require('./messenger/messages')
+
 var router = express.Router();
 
 router.get('/', function(req,res){
     res.render("index")
 })
+
+router.get('/messenger', verification)
+
+router.post('/messenger', message)
 
 router.use(function(req,res){
     res.status(404);
