a303c68bc49414fafbd65b1ead41422cefcb2d74
unknown <phantomstation2012@gmail.com>
2019-02-17 22:13:14 +0800

    corrected the pagecount variable

--- routes.js
+++ routes.js
@@ -97,7 +97,7 @@ router.get('/', function(req,res){
     }
 })
 
-app.get('/pagecount', function (req, res) {
+router.get('/pagecount', function (req, res) {
     // try to initialize the db on every request if it's not already
     // initialized.
     if (!db) {
