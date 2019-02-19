a5d6a77f912f9346deb228c42a3e6edcfb40cc59
unknown <phantomstation2012@gmail.com>
2019-02-19 12:09:42 +0800

    updated the body-parser to use json

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 
-app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
 
 var staticPath = path.join(__dirname, "public");
 app.use(express.static(staticPath));
